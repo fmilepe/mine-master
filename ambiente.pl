cc6d618ae9cdda324f223250b67ca26df869b228
Felipe Milepe <fmilepe@gmail.com>
2016-06-12 16:42:32 -0300

    Fechando primeira parte do trabalho.

---
+++ ambiente.pl
@@ -0,0 +1,17 @@
+valor(3,3,1).
+valor(3,2,2).
+valor(3,1,1).
+valor(3,0,1).
+
+valor(2,2,2).
+valor(2,0,1).
+
+valor(1,3,1).
+valor(1,2,3).
+valor(1,1,2).
+valor(1,0,2).
+
+valor(0,3,0).
+valor(0,2,1).
+valor(0,0,1).
+
