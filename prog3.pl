59c700f680fa78a32021e8e0a50d5921077604b5
Felipe Milepe <fmilepe@gmail.com>
2016-07-04 20:32:52 -0300

    Mine Master 0.1 pronto.

--- prog3.pl
+++ prog3.pl
@@ -19,7 +19,7 @@ set_vizinhos(X,Y)   :- X1 is X - 1, X2 is X + 1, Y1 is Y - 1, Y2 is Y + 1, asser
 /* Invoca o set_vizinhos para cada casa do tabuleiro recursivamente */
 init_vizinhanca(X,Y) :- X=0,Y=0,!, set_vizinhos(0,0).
 init_vizinhanca(X,Y) :- Y=0,!, tamanho(T), T1 is T - 1, X1 is X - 1, set_vizinhos(X,Y),init_vizinhanca(X1,T1).
-init_vizinhanca(X,Y) :- set_vizinhos(X,Y), Y1 is Y - 1, init_vizinhanca(X,Y1),writeln(X).
+init_vizinhanca(X,Y) :- set_vizinhos(X,Y), Y1 is Y - 1, init_vizinhanca(X,Y1).
 
 /* Inicia as vizinhanças começando pela ultima casa do tabuleiro */
 init_vizinhos :- tamanho(T), T1 is T-1, init_vizinhanca(T1,T1).
@@ -44,7 +44,7 @@ prob_bomba_vizinho_list([V|L1],[P|L2]) :- prob_bomba_vizinho_list(L1,L2), prob_b
 prob_bomba_casa(Casa,P) :- lista_vizinhos(Casa,L), retira_vizinhos_abertos(L,L1), diff(L,L1,L2),prob_bomba_vizinho_list(L2,R), max_list(R,P).
 
 /* Varrendo todas as casas e achando a casa fechada com menor probabilidade de ter bomba, segundo a heurísitca escolhida */
-casa_menos_chance_bomba((X,Y),(-1,-1),0.999999999999999999) :- X < 0,!.
+casa_menos_chance_bomba((X,Y),(-1,-1),1.0) :- X < 0,!.
 
 casa_menos_chance_bomba((X,Y),(Z,W),PROB) :- Y > 0, v((X,Y),_),!, Y1 is Y - 1, casa_menos_chance_bomba((X,Y1),(V,S),P), Z is V, W is S, PROB is P.
 casa_menos_chance_bomba((X,Y),(Z,W),PROB) :- Y > 0, not(v((X,Y),_)), prob_bomba_casa((X,Y),P2),  Y1 is Y - 1, casa_menos_chance_bomba((X,Y1),(V,S),P), P2 > P,!, PROB is P, Z is V, W is S.
@@ -59,3 +59,11 @@ casa_menos_chance_bomba((X,Y),(Z,W),PROB) :- Y = 0, not(v((X,Y),_)), not(prob_bo
 casa_menos_chance_bomba(_,_,_).
 
 :- init_vizinhos.
+
+/* Cláusula que seleciona as jogadas */
+joga :- nb_getval(finish,FIM), not(FIM),tamanho(T), T1 is T - 1, casa_menos_chance_bomba((T1,T1),(X,Y),P), P < 1, X > -1,posicao(X,Y),joga.
+joga :- nb_getval(finish,FIM),not(FIM),tamanho(T),T1 is T-1, casa_menos_chance_bomba((T1,T1),COORD,P),COORD=(-1,-1) , random_between(0,T1,X),random_between(0,T1,Y),posicao(X,Y),joga.
+
+joga :- nb_getval(finish,FIM),not(FIM),tamanho(T),T1 is T-1, casa_menos_chance_bomba((T1,T1),COORD,P),P=1, random_between(0,T1,X),random_between(0,T1,Y),prob_bomba_casa((X,Y),P), P < 1, not(v((X,Y),_)), posicao(X,Y),joga.
+joga :- nb_getval(finish,FIM),not(FIM),tamanho(T),T1 is T-1, casa_menos_chance_bomba((T1,T1),COORD,P),P=1, random_between(0,T1,X),random_between(0,T1,Y),prob_bomba_casa((X,Y),P), P < 1, v((X,Y),_), joga.
+joga :- nb_getval(finish,FIM),not(FIM),tamanho(T),T1 is T-1, casa_menos_chance_bomba((T1,T1),COORD,P),P=1, random_between(0,T1,X),random_between(0,T1,Y),prob_bomba_casa((X,Y),P), P = 1,joga.
