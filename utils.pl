bc308a1ebed204fba52223864a9d3eb206bc61bc
Felipe Milepe de Souza <fmilepe@users.noreply.github.com>
2016-07-14 11:49:47 -0300

    Comentando código e deletando lixo.

--- utils.pl
+++ utils.pl
@@ -1,13 +1,8 @@
+/* Cláusula auxiliar que calcula o comprimento de uma lista. */
 len([],0).
 len([_|L],N1) :- len(L,N), N1 is N+1.
 
+/* Cláusula auxiliar que calcula a diferença entre duas listas. */
 diff([],_,[]).
 diff([X | L1], L2, [X|L3]) :- not(member(X,L2)),!,diff(L1,L2,L3).
 diff([_|L1],L2, L3) :- diff(L1,L2,L3).
-
-max(N1,N2,N3) :- N1> N2,!, N3 is N1.
-max(_,N2,N3) :- N3 is N2.
-
-maximo_lista([N],N).
-maximo_lista([P|L],Max) :- maximo_lista(L,Max), max(Max,P,R),Max is R.
-/*maximo_lista([_|L],Max) :- maximo_lista(L,Max).*/
